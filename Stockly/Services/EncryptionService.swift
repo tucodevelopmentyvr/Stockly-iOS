8554788e1e159730fdf799adc177f5b23e551e95
Mike <tucodevelopmentyvr@gmail.com>
2025-04-04 14:58:16 +0330

    Fix EncryptionService by adding CommonCrypto import and fixing unused variable

--- Stockly/Services/EncryptionService.swift
+++ Stockly/Services/EncryptionService.swift
@@ -1,5 +1,6 @@
 import Foundation
 import CryptoKit
+import CommonCrypto
 
 enum EncryptionError: Error {
     case encryptionFailed
@@ -16,7 +17,7 @@ class EncryptionService {
     private let ivLength = 16
     private let pbkdf2Iterations = 100000
     private let pbkdf2KeyLength = 32 // 256 bits
-    
+
     /// Encrypt data with a password
     /// - Parameters:
     ///   - data: Data to encrypt
@@ -26,24 +27,24 @@ class EncryptionService {
         // Generate random salt and IV
         let salt = generateRandomBytes(length: saltLength)
         let iv = generateRandomBytes(length: ivLength)
-        
+
         // Derive key from password and salt
         guard let key = deriveKey(fromPassword: password, salt: salt) else {
             throw EncryptionError.keyDerivationFailed
         }
-        
+
         // Create a symmetric key from the derived key
         let symmetricKey = SymmetricKey(data: key)
-        
+
         do {
             // Encrypt the data
             let sealedBox = try AES.GCM.seal(data, using: symmetricKey, nonce: AES.GCM.Nonce(data: iv))
-            
+
             // Combine salt, IV, and encrypted data
             var encryptedData = Data()
             encryptedData.append(salt)
             encryptedData.append(iv)
-            
+
             if let combined = sealedBox.combined {
                 encryptedData.append(combined)
                 return encryptedData
@@ -54,7 +55,7 @@ class EncryptionService {
             throw EncryptionError.encryptionFailed
         }
     }
-    
+
     /// Decrypt data with a password
     /// - Parameters:
     ///   - encryptedData: Encrypted data with salt and IV prepended
@@ -65,24 +66,26 @@ class EncryptionService {
         guard encryptedData.count > saltLength + ivLength else {
             throw EncryptionError.invalidData
         }
-        
+
         // Extract salt, IV, and encrypted data
         let salt = encryptedData.subdata(in: 0..<saltLength)
-        let iv = encryptedData.subdata(in: saltLength..<(saltLength + ivLength))
+        // We extract the IV but it's actually included in the sealedBoxData for AES.GCM.SealedBox
+        // so we don't need to use it separately
+        _ = encryptedData.subdata(in: saltLength..<(saltLength + ivLength))
         let sealedBoxData = encryptedData.subdata(in: (saltLength + ivLength)..<encryptedData.count)
-        
+
         // Derive key from password and salt
         guard let key = deriveKey(fromPassword: password, salt: salt) else {
             throw EncryptionError.keyDerivationFailed
         }
-        
+
         // Create a symmetric key from the derived key
         let symmetricKey = SymmetricKey(data: key)
-        
+
         do {
             // Create a sealed box from the encrypted data
             let sealedBox = try AES.GCM.SealedBox(combined: sealedBoxData)
-            
+
             // Decrypt the data
             let decryptedData = try AES.GCM.open(sealedBox, using: symmetricKey)
             return decryptedData
@@ -90,7 +93,7 @@ class EncryptionService {
             throw EncryptionError.decryptionFailed
         }
     }
-    
+
     /// Generate random bytes of specified length
     /// - Parameter length: Number of bytes to generate
     /// - Returns: Data containing random bytes
@@ -99,7 +102,7 @@ class EncryptionService {
         _ = SecRandomCopyBytes(kSecRandomDefault, length, &bytes)
         return Data(bytes)
     }
-    
+
     /// Derive a key from a password and salt using PBKDF2
     /// - Parameters:
     ///   - password: Password to derive key from
@@ -109,7 +112,7 @@ class EncryptionService {
         guard let passwordData = password.data(using: .utf8) else {
             return nil
         }
-        
+
         var derivedKeyData = Data(repeating: 0, count: pbkdf2KeyLength)
         let derivationStatus = derivedKeyData.withUnsafeMutableBytes { derivedKeyBytes in
             salt.withUnsafeBytes { saltBytes in
@@ -125,7 +128,7 @@ class EncryptionService {
                 }
             }
         }
-        
+
         return derivationStatus == kCCSuccess ? derivedKeyData : nil
     }
 }
